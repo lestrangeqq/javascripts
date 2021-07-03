ba3bc74fec8445d6d108a08240af9fde89c4f19f
Alex <lestraeqq@gmail.com>
2021-07-03 11:57:07 +0300

    Update collect_ids.js

--- collect_ids.js
+++ collect_ids.js
@@ -1,7 +1,7 @@
 //Скажем, на странице есть много div с одним классом .questions но с разными id. 
 //Если возникла необходимость получить названия всех этих id можно использовать этот скрипт.
 
-let x = document.querySelectorAll('questions');
+let x = document.querySelectorAll('.questions');
 
 for (i = 0; i < x.length; i++) {
 	document.write(x[i].id);

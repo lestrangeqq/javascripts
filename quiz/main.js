fabcfbb45169133c5d56e6745b34240adb2f40e8
Alex <lestraeqq@gmail.com>
2021-07-08 08:42:46 +0300

    Update main.js

--- quiz/main.js
+++ quiz/main.js
@@ -1,20 +1,33 @@
+//Вопросы
+
 let x = [{
     q: 'hello',
     a1: 'ye',
-    a2: 'naw'
+    a2: 'naw',
+    right: 'a1'
   },
 
   {
     q: 'hello2',
     a1: 'ye2',
-    a2: 'naw2'
+    a2: 'naw2',
+    right: 'a2'
   }
-]
+];
+
+//Вопросы: конец.
+
+//Получаем рандомный вопрос
+
+let qRandom = x[Math.floor(Math.random() * x.length)];
+let qNumber = x.indexOf(qRandom);
+
+//HTML
 
 let qDesc = document.getElementById('desc');
 let answer1 = document.getElementById('a1');
 let answer2 = document.getElementById('a2');
 
-qDesc.innerHTML = x[1].q;
-answer1.innerHTML = x[1].a1;
-answer2.innerHTML = x[1].a2;
+qDesc.innerHTML = x[qNumber].q;
+answer1.innerHTML = x[qNumber].a1;
+answer2.innerHTML = x[qNumber].a2;
